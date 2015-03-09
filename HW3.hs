35c653ed419bdd23dc6812d9cb62eccbeb9518f0
Curtis Gagliardi <curtis@curtis.io>
2015-03-08 20:42:34 -0700

    working solutions for hw3

--- HW3.hs
+++ HW3.hs
@@ -1,11 +1,39 @@
 module HW3 where
 
+import qualified Data.Map.Strict as M
+import Data.List
+
 skips :: [a] -> [[a]]
 skips [] = []
-skips xs = map (\n -> takeNths n xs) [1..length xs]
+skips xs = map (`takeNths` xs) [1..length xs]
 
 
 takeNths :: Int -> [a] -> [a]
 takeNths _ [] = []
 takeNths n xs = let rest = drop (n - 1) xs
                 in take 1 rest ++ takeNths n (drop 1 rest)
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima []    = []
+localMaxima [_]   = []
+localMaxima [_,_] = []
+localMaxima (x:y:z:xs)
+  | y > x && y > z = y : localMaxima (y:z:xs)
+  | otherwise = localMaxima (y:z:xs)
+
+histogram :: [Integer] -> String
+histogram [] = intercalate "\n" ["==========", "0123456789"]
+histogram ints = intercalate "\n" $ render freqMap ++ ["==========", "0123456789"]
+  where
+    freqMap = freqs ints
+    render m = go (maximum . map snd $ M.toList m)
+    go 0 = []
+    go n = concatMap
+            (\k -> if M.findWithDefault 0 k freqMap >= n then "*" else " ")
+            [0..9]
+           : go (n - 1)
+
+freqs :: (Eq a, Ord a) => [a] -> M.Map a Int
+freqs = foldl' buildMap M.empty
+  where
+    buildMap m k = M.insertWith (+) k 1 m
--- test/test.hs
+++ test/test.hs
@@ -1,13 +1,38 @@
 import Test.Tasty
 import Test.Tasty.HUnit
+import Test.Tasty.Hspec
+
+import qualified  Data.Map.Strict as M
 
 import HW3
 
-main :: IO ()
-main = defaultMain tests
 
-tests :: TestTree
-tests = testGroup "Tests" [unitTests]
+main :: IO ()
+main = do
+  testTree <- tests
+  defaultMain testTree
+
+tests :: IO TestTree
+tests = do
+  hspecSuite <- testSpec "HSpec Unit Tests" hspecTests
+  return $ testGroup "Tests" [unitTests, hspecSuite]
+
+hspecTests :: SpecWith ()
+hspecTests = do
+  describe "Local maximum (Problem 2)" $
+    it "finds local maxima" $ do
+      shouldBe (localMaxima [2,9,5,6,1]) [9,6]
+      shouldBe (localMaxima [2,3,4,1,5]) [4]
+      shouldBe (localMaxima [1,2,3,4,5]) []
+  describe "Histogram" $ do
+    describe "freqs" $
+      it "builds a frequency map" $
+        shouldBe (freqs [1 :: Integer,2,3,1]) (M.fromList [(1,2), (2, 1), (3, 1)])
+    describe "rendering" $ do
+        -- TODO: assertions instead of visual inspection
+        runIO $ putStrLn (histogram [1,1,1,5])
+        runIO $ putStrLn (histogram [1,4,5,4,6,6,3,4,2,4,9])
+
 
 unitTests :: TestTree
 unitTests =
